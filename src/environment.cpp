0d830bbe6cad3b4bcf46e66266a552cc8e0c9b79
Bjarni Thor <bjarni@bjarnithor.com>
2022-02-28 12:03:00 +0000

    Environment can now generate all possible moves from current state for a given player.

--- src/environment.cpp
+++ src/environment.cpp
@@ -1,5 +1,9 @@
 #include "environment.hpp"
 
+#define COUTRESET "\033[0m"
+#define COUTBLACK "\033[1m\033[30m"
+#define COUTBLUE "\033[1m\033[34m"
+
 Cell::Cell() {}
 Cell::Cell(int x, int y, std::string piece, std::string player, DFAState *state)
     : x(x), y(y), piece(piece), player(player), state(state) {}
@@ -9,7 +13,7 @@ Environment::Environment(int board_size_x, int board_size_y) : board_size_x(boar
 Environment::~Environment() {}
 
 bool Environment::contains_cell(int x, int y) {
-    return 0 <= x && x <= board_size_x && 0 <= y && y <= board_size_y;
+    return 0 <= x && x < board_size_x && 0 <= y && y < board_size_y;
 }
 
 int Environment::set_cell(int x, int y, Cell *cell) {
@@ -29,6 +33,48 @@ Cell *Environment::get_cell(int x, int y) {
     return &board[x][y];
 }
 
+void Environment::generate_moves(std::string player) {
+    current_player = player;
+    found_moves.clear();
+    for (int i = 0; i < board_size_x; i++) {
+        for (int j = 0; j < board_size_y; j++) {
+            if (board[i][j].player == player) {
+                candidate_move.clear();
+                candidate_move.push_back({i, j});
+                generate_moves(board[i][j].state, i, j);
+            }
+        }
+    }
+}
+
+void Environment::generate_moves(DFAState *state, int x, int y) {
+    if (state->is_accepting)
+        found_moves.push_back(candidate_move);
+    for (auto &p : state->transition) {
+        const DFAInput &input = p.first;
+        int next_x = x - input.dx;
+        int next_y = y + input.dy;
+        if (!contains_cell(next_x, next_y))
+            continue;
+        if (!verify_predicate(input.predicate, next_x, next_y))
+            continue;
+        candidate_move.push_back({next_x, next_y});
+        generate_moves(p.second, next_x, next_y);
+        candidate_move.pop_back();
+    }
+}
+
+bool Environment::verify_predicate(std::string predicate, int x, int y) {
+    if (predicate == "empty")
+        return board[x][y].piece == "empty";
+    else if (predicate == "opponent")
+        return board[x][y].player != "" && board[x][y].player != current_player;
+    else {
+        std::cout << "Unknown predicate " << predicate << std::endl;
+        return false;
+    }
+}
+
 void Environment::print() {
     for (int i = 0; i < board_size_x; i++) {
         if (i != 0)
@@ -36,7 +82,12 @@ void Environment::print() {
         for (int j = 0; j < board_size_y; j++) {
             if (j != 0)
                 std::cout << " ";
-            std::cout << board[i][j].piece;
+            if (board[i][j].player == "white")
+                std::cout << COUTBLUE << board[i][j].piece << COUTRESET;
+            else if (board[i][j].player == "black")
+                std::cout << COUTBLACK << board[i][j].piece << COUTRESET;
+            else
+                std::cout << board[i][j].piece;
         }
         std::cout << "\n";
     }
--- src/environment.hpp
+++ src/environment.hpp
@@ -26,8 +26,16 @@ class Environment
     int board_size_x;
     int board_size_y;
     std::vector<std::vector<Cell>> board;
+    std::vector<std::vector<std::pair<int, int>>> found_moves;
     bool contains_cell(int x, int y);
     int set_cell(int x, int y, Cell *cell);
     Cell *get_cell(int x, int y);
+    void generate_moves(std::string player);
     void print();
+
+  private:
+    std::string current_player;
+    void generate_moves(DFAState *state, int x, int y);
+    std::vector<std::pair<int, int>> candidate_move;
+    bool verify_predicate(std::string predicate, int x, int y);
 };
